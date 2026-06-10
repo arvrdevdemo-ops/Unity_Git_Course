b51d97bd218142f29512573054cf12cee7438111
arvrdevdemo-ops <arvrdevdemo@gmail.com>
2026-06-10 16:35:57 +0530

    Revert "Add player prefab and initialize movement script"
    This reverts commit 4f06477a1438814b824b12ea8b4675c0155c780b.

--- Unity_Git_Course/Assets/Playermovement.cs
+++ Unity_Git_Course/Assets/Playermovement.cs
@@ -7,7 +7,7 @@ public class Playermovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Player has initialized!");
+        
     }
 
     // Update is called once per frame
